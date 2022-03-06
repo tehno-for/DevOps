f09ea08f33e31d8bb689b6e7476dde0a2530da85
tehno-for <keoroot@gmail.com>
2022-03-06 16:15:15 +0400

    RSpec with port issue

--- californiya/spec/unit/page_spec.rb
+++ californiya/spec/unit/page_spec.rb
@@ -3,8 +3,24 @@ require 'capybara/dsl'
 require 'selenium-webdriver'
 
 include Capybara::DSL
-Capybara.register_driver :selenium. do |app|
+Capybara.app_host = "http://website:8000"
+Capybara.server_host = "localhost"
+Capybara.server_port = "8000"
+Capybara.run_server = false
+Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(
+        app,
+        :browser => :remote,
+        :url => "http://#{ENV['SELENIUM_HOST']}:#{ENV['SELENIUM_PORT']}/wd/hub",
+        :desired_capabilities => Selenium::WebDriver::Remote::Capabilities.chrome(
+            "chromeOptions" => {"args" => 
+                ['--no-default-browser-check',
+                '--disable-dev-shm-usage']
+            }
+        )
+    )
 end
+Capybara.default_driver = :selenium
 
 describe "Example page rander unit test " do
     it "Should show the Explore Californiya " do 
